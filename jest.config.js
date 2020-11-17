5ceffe8cc0daf5bcbda644c4fe4051389f1a86fd
David Baxter <baxter30@gmail.com>
2020-11-17 14:57:38 -0700

    added api

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '<rootDir>/libs/store/ui-shared',
     '<rootDir>/libs/store/util-formatters',
     '<rootDir>/libs/store/feature-game-detail',
+    '<rootDir>/apps/api',
   ],
 };

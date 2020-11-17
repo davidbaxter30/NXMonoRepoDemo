9a44da15e7641a4178e1a33c5ae78b108572e907
David Baxter <baxter30@gmail.com>
2020-11-17 14:03:38 -0700

    added utilities lib

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/store',
     '<rootDir>/libs/libs/store/ui-shared',
     '<rootDir>/libs/store/ui-shared',
+    '<rootDir>/libs/store/util-formatters',
   ],
 };
